c4592063ed974261a7bb931a2123c43cdd59b12f
Wiktor <frelich.wiktor@gmail.com>
2023-12-11 17:36:42 +0100

    Creating a new constructors and new tests for landing page.

--- pages/landingPage.js
+++ pages/landingPage.js
@@ -1,12 +1,16 @@
 import { test, expect } from "@playwright/test";
 
 exports.landingPage = class landingPage {
+
   constructor(page) {
     this.page = page;
     this.searchbox_textbox = page.locator('#search_query_field_desktop')
     this.searchbox_button = page.locator('[type="submit"][data-zta="search_form_button_desktop"]')
     this.checkout_button = page.locator('data-testid="MiniCartLink')
     this.popup_accept_button = page.locator('#onetrust-accept-btn-handler')
+    this.contact_button = page.locator('#srh-help')
+    this.lowerbar_module = page.locator('#header-category-links')
+    this.brandlogo_image = page.locator('#srh-logo')
   }
 
   async goto() {
@@ -14,9 +18,19 @@ exports.landingPage = class landingPage {
   }
 
   async acceptCookies() {
-    await expect(this.page.locator("#onetrust-accept-btn-handler")).toBeVisible()
-    await this.page.locator("#onetrust-accept-btn-handler").click()
+    const popupAcceptButton = this.popup_accept_button;
+    await expect(popupAcceptButton).toBeVisible()
+    await popupAcceptButton.click()
   }
+
+  async pageTitleIsCorrect(){
+    await expect(this.page).toHaveTitle('Internetowy sklep zoologiczny: karma i akcesoria dla zwierząt w zooplus')
+  }
+
+  // async websiteImagesAreLoadedCorrect(){
+  //   const brandLogoImage = this.brandlogo_image
+  //   await expect(brandLogoImage).toBeVisible
+  // }
 
   async searchboxFill(product) {
     await this.searchbox_textbox.fill(product)
--- tests/landingPage.spec.js
+++ tests/landingPage.spec.js
@@ -1,5 +1,11 @@
 import { test, expect } from "@playwright/test";
-import { landingPage } from "../pages/mainPage.js";
+import { landingPage } from "../pages/landingPage.js";
+
+test ('Page title is correct', async ({page}) =>{
+    const Landing = new landingPage(page)
+    await Landing.goto()
+    await Landing.pageTitleIsCorrect();
+})
 
 test("Searchbox is working", async ({ page }) => {
     const Landing = new landingPage(page)
@@ -7,6 +13,13 @@ test("Searchbox is working", async ({ page }) => {
     await Landing.acceptCookies()
     await Landing.searchboxFill('Purizon')
   })
-;
+
+// test('test', async ({page}) =>{
+//     const Landing = new landingPage(page)
+//     await Landing.goto()
+//     await page.pause()
+// })
+
+
 
 

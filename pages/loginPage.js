5ce7e686db9c46017e4ceb32553ee86f13c50028
Wiktor <frelich.wiktor@gmail.com>
2024-01-25 22:37:56 +0100

    Fixing the tests.

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -7,16 +7,25 @@ exports.loginPage = class loginPage {
     this.page = page;
     this.popup_accept_button = page.locator('#onetrust-accept-btn-handler')
     this.logo_image = page.locator('[class="logo"]')
+    this.form_login = page.locator('[class="form-login"')
+    this.active_tab_item = page.locator('[class="tab-item-active"]')
+    this.form_header = page.locator('[class="form-title"]')
     this.username_input = page.locator('#username')
     this.password_input = page.locator('#password')
     this.passwordVisibilityEnable_button = page.locator ('#password-visibility-enable')
     this.passwordVisibilityDisable_button = page.locator ('#password-visibility-disable')
     this.login_button = page.locator('#login-btn')
-    this.forgotpassword_button = page.locator('#forgot-password-link')
+    this.forgotPassword_button = page.locator('#forgot-password-link')
     this.invalidLoginData = page.locator('[class="message-box message-box--error"]')
+
+    //register form
+    this.form_register = page.locator('[class="form-register"')
+    this.firstName_input = page.locator('#firstName')
+    this.lastName_input = page.locator('#lastName')
+    this.email_input = page.locator('#email')
   }
 
-  async goto() {
+  async goToPage() {
     await this.page.goto("https://login.zooplus.pl/auth/realms/zooplus/protocol/openid-connect/auth?response_type=code&client_id=shop-myzooplus-prod-zooplus&redirect_uri=https://www.zooplus.pl/web/sso-myzooplus/login&state=18f790f8-704a-41b1-8e2c-d3dafd27b4bf&login=true&ui_locales=pl-PL&scope=openid")
   }
 
@@ -27,7 +36,33 @@ exports.loginPage = class loginPage {
   }
 
   async pageTitleIsCorrect(){
-    await expect(this.page).toHaveTitle('Zaloguj się do zooplus')
+    await expect(this.page).toHaveTitle("Zaloguj się do zooplus")
+  }
+
+  async pageHeaderTextIsCorrect(){
+    await expect(this.form_header).toHaveText("Obecny Klient")
+  }
+
+  async loginFormIsCorrect(){
+    await expect(this.login_form).toBeVisible
+    await expect(this.active_tab_item).toHaveText("Login")
+  }
+
+  async switchToRegisterForm(){
+    await this.page.getByRole('link', { name: 'Rejestracja' }).click()
+    await expect(this.active_tab_item).toHaveText("Rejestracja")
+  }
+
+  async registerCustomer(firstname, lastname, email, password){
+    await this.firstName_input.fill(firstname)
+    await this.lastName_input.fill(lastname)
+    await this.email_input.fill(email)
+    await this.password_input.fill(password)
+    await this.login_button.click()
+    // zooplus has some bot-blocker tool which is showing a "there's an error message, but i found a way to go through it :)"
+    await this.page.getByRole('link', { name: 'Zooplus' }).click()
+    await this.page.locator('#shop-header').getByRole('link', { name: 'Mój zooplus' }).click()
+    await expect(this.page.locator('[data-zta="welcome-section-title"]')).toHaveText(`Cześć, ${firstname}!`);
   }
 
   async invalidDataLogin(login, password){
--- tests/loginPage.spec.js
+++ tests/loginPage.spec.js
@@ -1,16 +1,29 @@
 import { test, expect } from "@playwright/test";
 import { loginPage } from "../pages/loginPage.js";
 
+test.beforeEach('Setup - entering a page and accepting a cookies before every test', async ({page}) => {
+    const Login = new loginPage(page)
+    await Login.goToPage()
+    await Login.acceptCookies()
+  });
+
 test ('Page title is correct', async ({page}) =>{
     const Login = new loginPage(page)
-    await Login.goto()
     await Login.pageTitleIsCorrect();
+})
+
+test ('Login page header text is correct', async ({page}) =>{
+    const Login = new loginPage(page)
+    await Login.pageHeaderTextIsCorrect();
+})
+
+test ('Login form is visible', async ({page}) =>{
+    const Login = new loginPage(page)
+    await Login.loginFormIsCorrect();
 })
 
 test ('Login with incorrect data', async ({page}) =>{
     const Login = new loginPage(page)
-    await Login.goto()
-    await Login.acceptCookies()
     await Login.invalidDataLogin('login@test.com', 'test123!');
 })
 
@@ -20,12 +33,16 @@ test ('Login with incorrect data', async ({page}) =>{
 
 test ('Login with correct data', async ({page}) =>{
     const Login = new loginPage(page)
-    await Login.goto()
-    await Login.acceptCookies()
     await Login.validDataLogin('test.wiktor.email@gmail.com', 'Qwerty123!')
 })
 
+test ('Register form is visible', async ({page}) =>{
+    const Login = new loginPage(page)
+    await Login.switchToRegisterForm();
+})
 
-
-
-
+test ('Register a new customer', async ({page}) =>{
+    const Login = new loginPage(page)
+    await Login.switchToRegisterForm();
+    await Login.registerCustomer('firstname', 'lastname', 'testingemail2024@test.com', 'password1234!');
+})

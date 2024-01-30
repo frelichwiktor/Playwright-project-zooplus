1c2b27d3757edb34cc1fb6d97d27d0de793e738c
Robert <robert.babinski95@gmail.com>
2024-01-30 20:10:12 +0100

    Babiaggio feat. Freliggio - Playwright King Wanna Be
    - added ESLint with Prettier and Playwright plugins
    - added Prettier
    - added Prettier & ESLint config files
    - added JSDoc examples
    - updated project deps
    - updated .gitignore with .idea path for JetBrains IDEs
    - fixed all code formatting issues

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,84 +1,125 @@
-import { test, expect } from "@playwright/test";
-import exp from "constants";
+import { test, expect } from '@playwright/test';
+import exp from 'constants';
+
+// JSDoc Example v1
+/**
+ * @property {Object} page - The page object from Playwright.
+ * @property {Object} popup_accept_button - Locator for the accept button in the popup.
+ * @property {Object} logo_image - Locator for the logo image.
+ * @property {Object} form_login - Locator for the login form.
+ */
 
 exports.loginPage = class loginPage {
-
   constructor(page) {
+    // JSDoc Examples v2
+    /**
+     * @property {Object} page - The page object from Playwright.
+     */
     this.page = page;
-    this.popup_accept_button = page.locator('#onetrust-accept-btn-handler')
-    this.logo_image = page.locator('[class="logo"]')
-    this.form_login = page.locator('[class="form-login"')
-    this.active_tab_item = page.locator('[class="tab-item-active"]')
-    this.form_header = page.locator('[class="form-title"]')
-    this.username_input = page.locator('#username')
-    this.password_input = page.locator('#password')
-    this.passwordVisibilityEnable_button = page.locator ('#password-visibility-enable')
-    this.passwordVisibilityDisable_button = page.locator ('#password-visibility-disable')
-    this.login_button = page.locator('#login-btn')
-    this.forgotPassword_button = page.locator('#forgot-password-link')
-    this.invalidLoginData = page.locator('[class="message-box message-box--error"]')
+
+    /**
+     * @property {Object} popup_accept_button - Locator for the accept button in the popup.
+     */
+    this.popup_accept_button = page.locator('#onetrust-accept-btn-handler');
+
+    /**
+     * @property {Object} logo_image - Locator for the logo image.
+     */
+    this.logo_image = page.locator('[class="logo"]');
+
+    /**
+     * @property {Object} form_login - Locator for the login form.
+     */
+    this.form_login = page.locator('[class="form-login"');
+    this.active_tab_item = page.locator('[class="tab-item-active"]');
+    this.form_header = page.locator('[class="form-title"]');
+    this.username_input = page.locator('#username');
+    this.password_input = page.locator('#password');
+    this.passwordVisibilityEnable_button = page.locator(
+      '#password-visibility-enable',
+    );
+    this.passwordVisibilityDisable_button = page.locator(
+      '#password-visibility-disable',
+    );
+    this.login_button = page.locator('#login-btn');
+    this.forgotPassword_button = page.locator('#forgot-password-link');
+    this.invalidLoginData = page.locator(
+      '[class="message-box message-box--error"]',
+    );
 
     //register form
-    this.form_register = page.locator('[class="form-register"')
-    this.firstName_input = page.locator('#firstName')
-    this.lastName_input = page.locator('#lastName')
-    this.email_input = page.locator('#email')
+    this.form_register = page.locator('[class="form-register"');
+    this.firstName_input = page.locator('#firstName');
+    this.lastName_input = page.locator('#lastName');
+    this.email_input = page.locator('#email');
   }
 
   async goToPage() {
-    await this.page.goto("https://login.zooplus.pl/auth/realms/zooplus/protocol/openid-connect/auth?response_type=code&client_id=shop-myzooplus-prod-zooplus&redirect_uri=https://www.zooplus.pl/web/sso-myzooplus/login&state=18f790f8-704a-41b1-8e2c-d3dafd27b4bf&login=true&ui_locales=pl-PL&scope=openid")
+    await this.page.goto(
+      'https://login.zooplus.pl/auth/realms/zooplus/protocol/openid-connect/auth?response_type=code&client_id=shop-myzooplus-prod-zooplus&redirect_uri=https://www.zooplus.pl/web/sso-myzooplus/login&state=18f790f8-704a-41b1-8e2c-d3dafd27b4bf&login=true&ui_locales=pl-PL&scope=openid',
+    );
   }
 
   async acceptCookies() {
-    await expect(this.popup_accept_button).toBeVisible()
-    await this.popup_accept_button.click()
+    await expect(this.popup_accept_button).toBeVisible();
+    await this.popup_accept_button.click();
   }
 
-  async pageTitleIsCorrect(){
-    await expect(this.page).toHaveTitle("Zaloguj się do zooplus")
+  async pageTitleIsCorrect() {
+    await expect(this.page).toHaveTitle('Zaloguj się do zooplus');
   }
 
-  async pageHeaderTextIsCorrect(){
-    await expect(this.form_header).toHaveText("Obecny Klient")
+  async pageHeaderTextIsCorrect() {
+    await expect(this.form_header).toHaveText('Obecny Klient');
   }
 
-  async loginFormIsCorrect(){
-    await expect(this.login_form).toBeVisible
-    await expect(this.active_tab_item).toHaveText("Login")
+  async loginFormIsCorrect() {
+    await expect(this.login_form).toBeVisible;
+    await expect(this.active_tab_item).toHaveText('Login');
   }
 
-  async switchToRegisterForm(){
-    await this.page.getByRole('link', { name: 'Rejestracja' }).click()
-    await expect(this.active_tab_item).toHaveText("Rejestracja")
+  async switchToRegisterForm() {
+    await this.page.getByRole('link', { name: 'Rejestracja' }).click();
+    await expect(this.active_tab_item).toHaveText('Rejestracja');
   }
 
-  async registerCustomer(firstname, lastname, email, password){
-    await this.firstName_input.fill(firstname)
-    await this.lastName_input.fill(lastname)
-    await this.email_input.fill(email)
-    await this.password_input.fill(password)
-    await this.login_button.click()
+  async registerCustomer(firstname, lastname, email, password) {
+    await this.firstName_input.fill(firstname);
+    await this.lastName_input.fill(lastname);
+    await this.email_input.fill(email);
+    await this.password_input.fill(password);
+    await this.login_button.click();
     // zooplus has some bot-blocker tool which is showing a "there's an error message, but i found a way to go through it :)"
-    await this.page.getByRole('link', { name: 'Zooplus' }).click()
-    await this.page.locator('#shop-header').getByRole('link', { name: 'Mój zooplus' }).click()
-    await expect(this.page.locator('[data-zta="welcome-section-title"]')).toHaveText(`Cześć, ${firstname}!`);
+    await this.page.getByRole('link', { name: 'Zooplus' }).click();
+    await this.page
+      .locator('#shop-header')
+      .getByRole('link', { name: 'Mój zooplus' })
+      .click();
+    await expect(
+      this.page.locator('[data-zta="welcome-section-title"]'),
+    ).toHaveText(`Cześć, ${firstname}!`);
   }
 
-  async invalidDataLogin(login, password){
-    await this.username_input.fill(login)
-    await this.password_input.fill(password)
-    await this.page.pause()
-    await this.login_button.click()
-    await expect(this.invalidLoginData).toBeVisible()
+  async invalidDataLogin(login, password) {
+    await this.username_input.fill(login);
+    await this.password_input.fill(password);
+    await this.page.pause();
+    await this.login_button.click();
+    await expect(this.invalidLoginData).toBeVisible();
   }
 
-  async validDataLogin(login, password){
-    await this.username_input.fill(login)
-    await this.password_input.fill(password)
-    await this.login_button.click()
+  async validDataLogin(login, password) {
+    await this.username_input.fill(login);
+    await this.password_input.fill(password);
+    await this.login_button.click();
     //zooplus has some bot-blocker tool which is showing a "there's an error message, but i found a way to go through it :)"
-    await this.page.getByRole('link', { name: 'Zooplus' }).click()
-    await this.page.locator('#shop-header').getByRole('link', { name: 'Mój zooplus' }).click()
-    await expect(this.page.locator('[data-zta="welcome-section-title"]')).toHaveText('Cześć, test!')
+    await this.page.getByRole('link', { name: 'Zooplus' }).click();
+    await this.page
+      .locator('#shop-header')
+      .getByRole('link', { name: 'Mój zooplus' })
+      .click();
+    await expect(
+      this.page.locator('[data-zta="welcome-section-title"]'),
+    ).toHaveText('Cześć, test!');
   }
-}
+};
--- tests/landingPage.spec.js
+++ tests/landingPage.spec.js
@@ -1,78 +1,77 @@
-import { test, expect } from "@playwright/test";
-import { landingPage } from "../pages/landingPage.js";
+import { test, expect } from '@playwright/test';
+import { landingPage } from '../pages/landingPage.js';
 
-test.beforeEach('Setup - entering a page and accepting a cookies before every test', async ({page}) => {
-  const Landing = new landingPage(page)
-  await Landing.goToPage()
-  await Landing.acceptCookies()
+test.beforeEach(
+  'Setup - entering a page and accepting a cookies before every test',
+  async ({ page }) => {
+    const Landing = new landingPage(page);
+    await Landing.goToPage();
+    await Landing.acceptCookies();
+  },
+);
+
+test('Page title is correct', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.pageTitleIsCorrect();
 });
 
-test ('Page title is correct', async ({page}) =>{
-    const Landing = new landingPage(page)
-    await Landing.pageTitleIsCorrect();
-})
+test('Brand logo image is visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.brandLogoImageIsVisible();
+});
 
-test ('Brand logo image is visible', async ({page}) =>{
-  const Landing = new landingPage(page)
-  await Landing.brandLogoImageIsVisible()
-})
+test('Background images are visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.backgroundImagesAreVisible();
+});
 
-test ('Background images are visible', async ({page}) =>{
-    const Landing = new landingPage(page)
-    await Landing.backgroundImagesAreVisible()
-})
+test('Header module is visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.headerModuleIsVisible();
+});
 
-test("Header module is visible", async ({ page }) => {
-    const Landing = new landingPage(page)
-    await Landing.headerModuleIsVisible()
-  })
+test('Sale section is visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.saleSectionIsVisible();
+});
 
-  test("Sale section is visible", async ({ page }) => {
-    const Landing = new landingPage(page)
-    await Landing.saleSectionIsVisible()
-  })
+test('Sidebanner section is visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.sidebannerSectionIsVisible();
+});
 
-  test("Sidebanner section is visible", async ({ page }) => {
-    const Landing = new landingPage(page)
-    await Landing.sidebannerSectionIsVisible()
-  })
+test('Newsletter section is visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.newsletterSectionIsVisible();
+});
 
-  test("Newsletter section is visible", async ({ page }) => {
-    const Landing = new landingPage(page)
-    await Landing.newsletterSectionIsVisible()
-  })
+test('Info list section is visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.infolistSectionIsVisible();
+});
 
-  test("Info list section is visible", async ({ page }) => {
-    const Landing = new landingPage(page)
-    await Landing.infolistSectionIsVisible()
-  })
+test('Social and app section section is visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.socialAndAppSectionIsVisible();
+});
 
-  test("Social and app section section is visible", async ({ page }) => {
-    const Landing = new landingPage(page)
-    await Landing.socialAndAppSectionIsVisible()
-  })
+test('Purchase details section is visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.purchaseDetailsSectionIsVisible();
+});
 
-  test("Purchase details section is visible", async ({ page }) => {
-    const Landing = new landingPage(page)
-    await Landing.purchaseDetailsSectionIsVisible()
-  })
+test('Legal link section is visible', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.legalLinkSectionIsVisible();
+});
 
-  test("Legal link section is visible", async ({ page }) => {
-    const Landing = new landingPage(page)
-    await Landing.legalLinkSectionIsVisible()
-  })
-
-  test("Searchbox is working", async ({ page }) => {
-    const Landing = new landingPage(page)
-    await Landing.searchboxFill('Purizon')
-  })
+test('Searchbox is working', async ({ page }) => {
+  const Landing = new landingPage(page);
+  await Landing.searchboxFill('Purizon');
+});
 
 // test('test', async ({page}) =>{
 //     const Landing = new landingPage(page)
 //     await Landing.goto()
 //     await page.pause()
 // })
-
-
-
-
